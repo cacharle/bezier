5f585a60f70371089e884e53058693af1577f4ca
Charles Cabergs <me@cacharle.xyz>
2022-01-08 19:27:15 +0100

    Added dragging control points, adding/deleting them and README screenshot and instructions

---
+++ README.md
@@ -0,0 +1,33 @@
+# Bezier
+
+![screenshot](./screenshot.png)
+
+Visualization of a [Bézier curve][1] in Scheme.
+
+Inpired by a [Coding train video][2].
+
+## Usage
+
+Install [Chicken Scheme][3] and the [SDL2 bindings][4].
+
+```
+$ chicken-install sdl2  # takes 5min
+```
+
+Compile and run:
+
+```
+$ chicken -O5 bezier.scm
+$ ./bezier
+```
+
+## Keybindings
+
+* `Escape` or `q` to quit
+* Click on a control point (little square) to drag it around
+* `Up`/`Down` to add/remove control point
+
+[1]: https://en.wikipedia.org/wiki/B%C3%A9zier_curve
+[2]: https://www.youtube.com/watch?v=enNfb6p3j_g
+[3]: http://code.call-cc.org/
+[4]: http://wiki.call-cc.org/eggref/5/sdl2
--- bezier.scm
+++ bezier.scm
@@ -1,14 +1,11 @@
+(import srfi-1)
 (import (prefix sdl2 "sdl2:"))
-
-; (set! (sdl2:renderer-draw-blend-mode renderer) ...)
 
 (define *points-count* 100)
 
 (define *control-points*
   (map (lambda (p) (apply sdl2:make-point p))
-       '((10 200) (200 10) (400 400) (500 410))))
-
-(print *control-points*)
+       '((10 500) (300 10) (590 500))))
 
 ; https://en.wikipedia.org/wiki/Bezier_curve
 (define (bezier-point controls t)
@@ -20,18 +17,14 @@
           (sdl2:point-scale (bezier-point init t) (- 1.0 t))
           (sdl2:point-scale (bezier-point tail t) t)))))
 
-; (print (bezier-point *control-points* 0.5))
-
+; PERF: need memoization otherwise we're recomputing the same combinations multiple times (like fibonacci)
 (define (bezier-points controls points-count)
   (do ((t 0.0 (+ t (/ 1.0 points-count)))
        (points '()))
       ((>= t 1.0) points)
       (set! points (cons (bezier-point controls t) points))))
 
-; (print (bezier-points *control-points* 3))
-
 (sdl2:set-main-ready!)
-
 (sdl2:init! '(video))
 (on-exit sdl2:quit!)
 
@@ -47,12 +40,35 @@
 (define +background-color+ (sdl2:make-color 32 32 32))
 (define +lines-color+ (sdl2:make-color 200 200 200))
 
+(define +hit-rect-size+ 10)
+(define +hit-rect-size-half+ (/ +hit-rect-size+ 2))
+
+(define *selected-control-point-index* -1)
+
+(define (control-hit-rect point)
+  (let ((x (sdl2:point-x point))
+        (y (sdl2:point-y point)))
+    (sdl2:make-rect
+      (- x +hit-rect-size-half+)
+      (- y +hit-rect-size-half+)
+      +hit-rect-size+
+      +hit-rect-size+)))
+
+(define (draw-control-points! control-points)
+  (if (null? control-points)
+    '()
+    (let* ((p (car control-points))
+           (rest (cdr control-points))
+           (rect (control-hit-rect p)))
+      (sdl2:render-fill-rect! renderer rect)
+      (draw-control-points! rest))))
+
 (define (update!)
   (set! (sdl2:render-draw-color renderer) +background-color+)
   (sdl2:render-clear! renderer)
   (set! (sdl2:render-draw-color renderer) +lines-color+)
-  ; (sdl2:render-draw-line! renderer 10 10 100 100)
   (sdl2:render-draw-lines! renderer (bezier-points *control-points* *points-count*))
+  (draw-control-points! *control-points*)
   (sdl2:render-present! renderer))
 
 (do () (#f '())
@@ -60,17 +76,29 @@
       (let ((event (sdl2:wait-event!)))
         (case (sdl2:event-type event)
           ((quit) (exit))
-          ; ((mouse-button-down)
-          ;    (sdl2:mouse-button-event-x event)
-          ;    (sdl2:mouse-button-event-y event)
-          ; ((mouse-button-up)
-          ;    ...
-          ; ((mouse-motion)
-          ;   (sdl2:mouse-motion-event-state event)
-          ;    ...
+          ((mouse-button-down)
+            (let* ((x (sdl2:mouse-button-event-x event))
+                   (y (sdl2:mouse-button-event-y event))
+                   (mouse-point (sdl2:make-point x y))
+                   (index (list-index (lambda (p)
+                                        (sdl2:point-in-rect? mouse-point (control-hit-rect p)))
+                                      *control-points*)))
+                (when index (set! *selected-control-point-index* index))))
+          ((mouse-button-up)
+             (set! *selected-control-point-index* -1))
+          ((mouse-motion)
+            (when (not (null? (sdl2:mouse-motion-event-state event)))
+              (let ((x (sdl2:mouse-motion-event-x event))
+                    (y (sdl2:mouse-motion-event-y event)))
+                (when (not (= -1 *selected-control-point-index*))
+                  (set! (list-ref *control-points* *selected-control-point-index*)
+                        (sdl2:make-point x y))))))
           ((key-down)
            (case (sdl2:keyboard-event-sym event)
-             ((escape q) (exit))))))
-      (update!)
-      (sdl2:delay! 10)))
-
+             ((escape q) (exit))
+             ((up)
+                (set! *control-points* (cons (sdl2:make-point 20 20) *control-points*)))
+             ((down)
+                (when (not (null? (cdr *control-points*)))
+                  (set! *control-points* (cdr *control-points*))))))))
+      (update!)))
